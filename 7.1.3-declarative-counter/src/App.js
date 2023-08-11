13b435caea35a7b283965f58235807372c5f51c3
Elva Plazola <elvaplazola@yahoo.com>
2023-08-10 21:26:02 -0700

    final addition

--- 7.1.3-declarative-counter/src/App.js
+++ 7.1.3-declarative-counter/src/App.js
@@ -3,8 +3,12 @@ import { useState } from 'react'
 
 function App() {
   let [ counter, changeCounter ] = useState(1)
+  window.changeCounter = changeCounter
   return (
+    <div>
     <h1>{counter}</h1>
+    <h4>{counter}</h4>
+    </div>
   )
 }
 
